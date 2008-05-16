bf4b9903c354c0373cca0a392882c7a3e7c15328
adam <adam@pkqk.net>
2008-05-16 17:49:14 +1000

    as url

--- scrobbler.rb
+++ scrobbler.rb
@@ -5,9 +5,15 @@ $lastfm = {
   :user => 'pkqk',
   :password => 'nufink'
 }
-$audioscrobbler = "http://post.audioscrobbler.com"
+AUDIOSCROBBLER_URL = "http://post.audioscrobbler.com"
 
 class Scrobbler
+
+  def initialize(config)
+    @config = config
+    @net = Net::HTTP.new(AUDIOSCROBBLER_URL,80)
+  end
+
   def handshake
     @timestamp = Time.now.utc.to_i
     params = {
@@ -16,11 +22,25 @@ class Scrobbler
       'c' => 'tst',
       'v' => '1.0',
     }.merge(
-      'u' => $lastfm[:user],
-      'a' => auth($lastfm[:password],@timestamp),
+      'u' => @config[:user],
+      'a' => auth(@config[:password],@timestamp),
       't' => @timestamp
     )
     qs = params.collect { |kv| kv.join('=') }.join('&')
+    response = @net.get("/?#{qs}")
+    if response.code == 200
+      info = response.body.split("\n")
+      code = info.shift
+      if code == "OK"
+        @token, @np_url, @submit_url = info
+      else
+        puts code
+        raise SystemExit, 1
+      end
+    else
+      puts "Handshake failed"
+      raise SystemExit, response.code
+    end
   end
 
   def now_playing
