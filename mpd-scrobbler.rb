a6cabfe3f84bc70ebfebc1f6a93ade9a0145bc26
adam <adam@pkqk.net>
2008-05-16 11:30:18 +1000

    mpd-client refactor

--- mpd-scrobbler.rb
+++ mpd-scrobbler.rb
@@ -7,32 +7,56 @@ class MpdScrobbler
   def run
     @mpd = MPD.new(*$config)
     @current_song = nil
-    @mpd.register_callback(method(:time_callback), MPD::TIME_CALLBACK)
+    @scrobbled_song = false
+    @mpd.register_callback(method(:song_change), MPD::CURRENT_SONG_CALLBACK)
+    @mpd.register_callback(method(:time_step), MPD::TIME_CALLBACK)
     @mpd.connect(true);
+    wait_for_mpd_thread
   end
-  
-  def time_callback(*args)
+
+  def song_change(song)
     if @mpd.current_song != @current_song
       @current_song = @mpd.current_song
-      puts "\r#{@current_song.inspect}"
+      @scrobbled_song = false
+      now_playing(@current_song)
     end
-    $stdout.write "\r"+args.join('/')
-    $stdout.flush
   end
-  
+
+  def time_step(time,total)
+    return if total == 0
+    $stdout.write "\r#{time}/#{total}"
+    $stdout.flush
+    unless total < 30
+      if time > 240 || time.to_f/total.to_f > 0.5
+        scrobble(@current_song) if @current_song.time == total.to_s
+      end
+    end
+  end
+
+  def scrobble(song)
+    return if @scrobbled_song || @current_song != song
+    @scrobbled_song = true
+    puts "\nWould scrobble #{fmt_song(song)}"
+  end
+
+  def now_playing(song)
+    puts "\nNow Playing: #{fmt_song(song)}"
+  end
+
+  def fmt_song(song)
+    "#{song.artist} - #{song.album} - #{song.title}"
+  end
+
   def wait_for_mpd_thread
     # assuming mpd thread is the only other one
     Thread.list.reject { |t| t == Thread.current }.first.join
   rescue Interrupt
   end
-  
+
   def stop
     @mpd.disconnect
     exit
   end
 end
 
-player = MpdScrobbler.new
-trap('TERM') { puts "term called"; player.stop }
-player.run
-player.wait_for_mpd_thread
+MpdScrobbler.new.run
